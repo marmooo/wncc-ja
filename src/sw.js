52f494ddad3bcf8dc96f7ae1c81ae44d86674587
marmooo <marmooo@users.noreply.github.com>
2022-07-24 09:06:34 +0900

    bump bootstrap from 5.1.3 to 5.2.0

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,11 @@
-var CACHE_NAME = "2022-04-30 10:15";
+var CACHE_NAME = "2022-07-24 00:46";
 var urlsToCache = [
   "/wncc-ja/",
   "/wncc-ja/index.js",
   "/wncc-ja/sql.js-httpvfs/sql-wasm.wasm",
   "/wncc-ja/sql.js-httpvfs/sqlite.worker.js",
   "/wncc-ja/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
 ];
 
 self.addEventListener("install", function (event) {
