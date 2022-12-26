373cc8659735c5ec8cd27d54e11f41f37b2ad786
marmooo <marmooo@users.noreply.github.com>
2022-12-26 19:09:48 +0900

    fix

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-import { MeCab } from "https://deno.land/x/deno_mecab/mod.ts";
-import { DB } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
+export { MeCab } from "https://deno.land/x/deno_mecab/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.3.0/mod.ts";
