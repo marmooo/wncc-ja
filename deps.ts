ba0a5b288138e97fa955cf166aa1b8a08cafac14
marmooo <marmooo@users.noreply.github.com>
2023-06-03 01:17:56 +0900

    cleanup

--- deps.ts
+++ deps.ts
@@ -1,2 +1,1 @@
-export { MeCab } from "https://deno.land/x/deno_mecab/mod.ts";
-export { Database } from "https://deno.land/x/sqlite3@0.7.2/mod.ts";
+export { Database } from "https://deno.land/x/sqlite3@0.9.1/mod.ts";
